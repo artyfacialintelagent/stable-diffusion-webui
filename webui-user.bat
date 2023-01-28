b7eb38615658a4ff2cd4ecbeac17d2886b0e44c9
artyfacialintelagent <artyfacialintelagent@gmail.com>
2023-01-28 21:29:10 +0100

    Add xformers

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --autolaunch --ckpt-dir "C:\Temp\stable diffusion stuff\models"
+set COMMANDLINE_ARGS= --xformers --autolaunch --ckpt-dir "C:\Temp\stable diffusion stuff\models"
 
 call webui.bat
