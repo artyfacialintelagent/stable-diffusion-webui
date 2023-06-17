f3276e3f7a28fa52d9968fb483764cbf9f6cf118
artyfacialintelagent <artyfacialintelagent@gmail.com>
2023-06-17 15:33:49 +0200

    Remove --autolaunch from webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -5,6 +5,6 @@ del C:\Users\niclas\AppData\Local\Temp\*.png
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers --autolaunch --disable-safe-unpickle --ckpt-dir "C:\Temp\stable diffusion stuff\models"
+set COMMANDLINE_ARGS= --xformers --disable-safe-unpickle --ckpt-dir "C:\Temp\stable diffusion stuff\models"
 
 call webui.bat
